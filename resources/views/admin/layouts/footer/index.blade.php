85d8c4bb9353690db8f66291b7960e295c1ea65c
Sandesh-Sunar <sandesh.sunar.s24@icp.edu.np>
2026-07-07 13:54:12 +0545

    Make room section image size small

--- resources/views/admin/layouts/footer/index.blade.php
+++ resources/views/admin/layouts/footer/index.blade.php
@@ -1,1 +1,24 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script>
+    toastr.options = {
+        "closeButton": true,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "timeOut": "3000"
+    };
+
+    @if(Session::has('success'))
+        toastr.success("{{ Session::get('success') }}");
+    @endif
+    
+    @if(Session::has('error'))
+        toastr.error("{{ Session::get('error') }}");
+    @endif
+    
+    @if($errors->any())
+        @foreach($errors->all() as $error)
+            toastr.error("{{ $error }}");
+        @endforeach
+    @endif
+</script>
--- resources/views/admin/layouts/header/index.blade.php
+++ resources/views/admin/layouts/header/index.blade.php
@@ -4,7 +4,8 @@
     <title>Admin Panel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
     @stack('js')
