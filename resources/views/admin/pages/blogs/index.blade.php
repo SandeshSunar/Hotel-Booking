f94778985891603b293b618c13141bb096ef3e16
Sandesh-Sunar <sandesh.sunar.s24@icp.edu.np>
2026-07-21 15:33:18 +0545

    feat: create admin blog management index view with list and actions.

--- resources/views/admin/pages/blogs/index.blade.php
+++ resources/views/admin/pages/blogs/index.blade.php
@@ -14,12 +14,7 @@
         </a>
     </div>
 
-    @if(session('success'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ session('success') }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    @endif
+ 
 
     <div class="card shadow border-0 rounded-4">
         <div class="card-body p-0">
